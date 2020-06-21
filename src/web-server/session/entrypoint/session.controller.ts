b3f1212f6caece4083e8c0219b1a8b1761c999db
Renan lucilio <RSL1219>
2020-06-21 09:22:16 -0300

    Add babel e eslint

--- src/web-server/session/entrypoint/session.controller.ts
+++ src/web-server/session/entrypoint/session.controller.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from "express";
 import { CreateSessionUsecase } from "../usecase/create-session.usecase";
 import { BaseClass } from "../../../model/base-class";
-import cacheManager from "../../../cache/cache-manager";
+import { FindSessionUsecase } from "../usecase/find-session.usecase";
 
 export class SessionController extends BaseClass {
     private createSessionUsecase = new CreateSessionUsecase();
+    private findSessionUsecase = new FindSessionUsecase();
     private routerManager = Router();
 
     constructor() {
@@ -13,6 +14,7 @@ export class SessionController extends BaseClass {
 
     get route() {
         this.createSession()
+        this.verifyExistSession();
         return this.routerManager
     }
 
@@ -23,12 +25,28 @@ export class SessionController extends BaseClass {
             try {
                 const response = this.createSessionUsecase.execute(req.body);
                 this.log.info(`Response ${JSON.stringify(response)}`);
-                res.status(200).send(response);
+                res.status(200).json(response);
             }
             catch (error) {
                 this.log.error(`Erro in request -> ${JSON.stringify(error)}`)
-                res.status(400).send(error);
+                res.status(400).json(error);
             }
         });
+    }
+
+    private verifyExistSession() {
+        this.routerManager.get('/find/:name', (request: Request, response: Response) => {
+            this.log.info(`Request body -> ${request.params.name}`);
+
+            try {
+                const res = this.findSessionUsecase.execute(request.params.name);
+                this.log.info(`Response ${JSON.stringify(res)}`);
+                response.status(200).json(res);
+            }
+            catch (error) {
+                this.log.error(`Erro in request -> ${JSON.stringify(error)}`)
+                response.status(400).json(error);
+            }
+        })
     }
 }
---
+++ src/web-server/session/usecase/find-session.usecase.ts
@@ -0,0 +1,19 @@
+import configManager from "../../../cache/cache-manager";
+import { ErrorBase } from "../../error/error-base";
+import { ErrorTypes } from "../../error/error-types";
+
+export class FindSessionUsecase {
+    constructor() {}
+
+    execute(nameSession: string) {
+        nameSession = nameSession.replace(/ /g, '_');
+
+        const result = configManager.get(nameSession);
+
+        if (!result) {
+            throw new ErrorBase('Session no exist', ErrorTypes.Role, result);
+        }
+
+        return result;
+    }
+}
