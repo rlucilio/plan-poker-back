1d94dd0ee215cb830340835260d194d27d2fdd93
Renan lucilio <RSL1219>
2020-06-22 09:00:34 -0300

    Add ts-node-dev

--- src/web-server/session/entrypoint/session.controller.ts
+++ src/web-server/session/entrypoint/session.controller.ts
@@ -1,52 +1,46 @@
-import { Router, Request, Response } from "express";
-import { CreateSessionUsecase } from "../usecase/create-session.usecase";
-import { BaseClass } from "../../../model/base-class";
-import { FindSessionUsecase } from "../usecase/find-session.usecase";
+import { Router } from 'express';
+import { CreateSessionUsecase } from '../usecase/create-session.usecase';
+import { BaseClass } from '../../../model/base-class';
+import { FindSessionUsecase } from '../usecase/find-session.usecase';
 
 export class SessionController extends BaseClass {
     private createSessionUsecase = new CreateSessionUsecase();
     private findSessionUsecase = new FindSessionUsecase();
     private routerManager = Router();
 
-    constructor() {
-        super();
+    get route () {
+      this.createSession();
+      this.verifyExistSession();
+      return this.routerManager;
     }
 
-    get route() {
-        this.createSession()
-        this.verifyExistSession();
-        return this.routerManager
+    private createSession () {
+      this.routerManager.post('/', (req, res) => {
+        this.log.info(`Request body -> ${JSON.stringify(req.body)}`);
+
+        try {
+          const response = this.createSessionUsecase.execute(req.body);
+          this.log.info(`Response ${JSON.stringify(response)}`);
+          res.status(200).json(response);
+        } catch (error) {
+          this.log.error(`Erro in request -> ${JSON.stringify(error)}`);
+          res.status(400).json(error);
+        }
+      });
     }
 
-    private createSession() {
-        this.routerManager.post("/", (req: Request, res: Response) => {
-            this.log.info(`Request body -> ${JSON.stringify(req.body)}`);
+    private verifyExistSession () {
+      this.routerManager.get('/find/:name', (request, response) => {
+        this.log.info(`Request body -> ${request.params.name}`);
 
-            try {
-                const response = this.createSessionUsecase.execute(req.body);
-                this.log.info(`Response ${JSON.stringify(response)}`);
-                res.status(200).json(response);
-            }
-            catch (error) {
-                this.log.error(`Erro in request -> ${JSON.stringify(error)}`)
-                res.status(400).json(error);
-            }
-        });
-    }
-
-    private verifyExistSession() {
-        this.routerManager.get('/find/:name', (request: Request, response: Response) => {
-            this.log.info(`Request body -> ${request.params.name}`);
-
-            try {
-                const res = this.findSessionUsecase.execute(request.params.name);
-                this.log.info(`Response ${JSON.stringify(res)}`);
-                response.status(200).json(res);
-            }
-            catch (error) {
-                this.log.error(`Erro in request -> ${JSON.stringify(error)}`)
-                response.status(400).json(error);
-            }
-        })
+        try {
+          const res = this.findSessionUsecase.execute(request.params.name);
+          this.log.info(`Response ${JSON.stringify(res)}`);
+          response.status(200).json(res);
+        } catch (error) {
+          this.log.error(`Erro in request -> ${JSON.stringify(error)}`);
+          response.status(400).json(error);
+        }
+      });
     }
 }
--- src/web-server/session/usecase/create-session.usecase.ts
+++ src/web-server/session/usecase/create-session.usecase.ts
@@ -1,34 +1,29 @@
-import { ICreateSessionModel } from "./model/create-session.model";
-import { ErrorBase } from "../../error/error-base";
-import { ErrorTypes } from "../../error/error-types";
-import { ISaveSessionEntity } from "./model/save-session.entity";
-import cacheManager from "../../../cache/cache-manager";
-
+import { ErrorBase } from '../../error/error-base';
+import { ErrorTypes } from '../../error/error-types';
+import cacheManager from '../../../cache/cache-manager';
+import { ICreateSessionModel } from './model/create-session.model';
+import { ISaveSessionEntity } from './model/save-session.entity';
 
 export class CreateSessionUsecase {
     private myCache = cacheManager;
 
-    constructor() { }
+    execute (model: ICreateSessionModel) {
+      if (!model || !model.name || !model.owner) { throw new ErrorBase('Request body invalid', ErrorTypes.Params, model); }
 
-    execute(model: ICreateSessionModel) {
-        if (!model || !model.name || !model.owner)
-            throw new ErrorBase("Request body invalid", ErrorTypes.Params, model);  
+      const newSession: ISaveSessionEntity = {
+        session: {
+          name: model.name.replace(/ /g, '_'),
+          ownerName: model.owner
+        },
+        users: [
+          { name: model.owner }
+        ]
+      };
 
-        const newSession: ISaveSessionEntity = {
-            session: {
-                name: model.name.replace(/ /g, "_"),
-                ownerName: model.owner
-            },
-            users: [
-                { name: model.owner }
-            ]
-        }
+      if (this.myCache.get(newSession.session.name)) { throw new ErrorBase('There is already a room with that name', ErrorTypes.Role, model); }
 
-        if (this.myCache.get(newSession.session.name))
-            throw new ErrorBase("There is already a room with that name", ErrorTypes.Role, model);  
-
-        newSession.session.expireInSeconds = +(process.env.expire_room || 1800);
-        this.myCache.set(newSession.session.name, newSession);
-        return newSession.session
+      newSession.session.expireInSeconds = +(process.env.expire_room || 1800);
+      this.myCache.set(newSession.session.name, newSession);
+      return newSession.session;
     }
 }
