4ffa275379ad0a541d4aeb73b4510cd029131a6e
Renan lucilio <RSL1219>
2020-06-23 08:09:04 -0300

    Refactor event connect

--- src/socket-server/room/usecase/connect-room.usecase.ts
+++ src/socket-server/room/usecase/connect-room.usecase.ts
@@ -2,14 +2,17 @@ import { Socket } from 'socket.io';
 import { ErrorBase } from '../../../error/error-base';
 import { ErrorTypes } from '../../../error/error-types';
 import cacheManager from '../../../cache/cache-manager';
-import { VerifyConnectionRoom } from './verify-connection-room.usecase';
 import { IConnectRoomModel } from './model/connect-room.model';
 import { IRoom } from '../../../model/interfaces/room';
+import { BaseClass } from '../../../model/base-class';
+import socketServer from '../../socket-server';
+import { EventsSocket } from '../../events-socket';
 
-export class ConnectRoomUsecase {
+export class ConnectRoomUsecase extends BaseClass {
   private cache = cacheManager;
 
   execute (socket: Socket) {
+    this.log.info('Execute');
     const model: IConnectRoomModel = {
       room: socket.handshake.query.room,
       user: socket.handshake.query.user
@@ -19,6 +22,33 @@ export class ConnectRoomUsecase {
     if (!room) { throw new ErrorBase('Room invalid', ErrorTypes.Params, { model, room }); }
 
     socket.join(room?.room.name);
-    new VerifyConnectionRoom().execute(room, model);
+    this.verifyConnectionRoom(room, model);
+  }
+
+  private verifyConnectionRoom (room: IRoom, model: IConnectRoomModel) {
+    if (room.users.find(user => user.name === model.user)) {
+      this.log.info(`Send event ${EventsSocket.joinRoom}`);
+
+      socketServer.socket?.in(room.name)
+        .emit(EventsSocket.joinRoom, {
+          msg: 'Entrou na sala',
+          user: model.user
+        });
+    } else {
+      this.log.info(`Send event ${EventsSocket.returnRoom}`);
+
+      socketServer.socket?.in(room.name)
+        .emit(EventsSocket.returnRoom, {
+          msg: 'Novo membro na sala',
+          user: model.user
+        });
+
+      room.users.push({
+        name: model.user,
+        owner: false
+      });
+
+      cacheManager.set(room.name, room);
+    }
   }
 }
